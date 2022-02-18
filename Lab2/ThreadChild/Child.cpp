29a273d6491600d60fd9b497338cce29a8d9b2e8
yanomaly <family.tolstoy@gmail.com>
2022-02-18 14:43:49 +0300

    Lab2 Initial commit

---
+++ Lab2/ThreadChild/Child.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int size = 1, elem, j = 0, k = 0;
+	
+	/*int* array = new int[size];
+	int* newArray = new int[size];
+	int* tempArray = new int[size];
+	for (int i = 0; i < size; i++)
+		if (array[i] == elem) {
+			newArray[j] = elem;
+			j++;
+		}
+		else{
+			tempArray[k] = array[i];
+			k++;
+		}
+	for (int i = j, l = 0; l < k; l++, i++)
+		newArray[i] = tempArray[l];
+	for (int i = 0; i < size; i++)
+		cout << newArray[i] << " ";*/
+	return 0;
+}
---
+++ Lab2/ThreadParent/Parent.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main() {
+	int size, elem;
+	wstring childName(L"ThreadChild.exe");
+	LPWSTR child = &childName[0];
+	string commandLine = "";
+	STARTUPINFO sti;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&sti, sizeof(STARTUPINFO));
+	sti.cb = sizeof(STARTUPINFO);
+	cout << "Input size of array: " << endl;
+	cin >> size;
+	int* array = new int[size];
+	cout << "Input " << size << " digits: " << endl;
+	for (int i = 0; i < size; i++)
+		cin >> array[i];
+	cout << "Input element: " << endl;
+	cin >> elem;
+	commandLine += elem;
+	commandLine += size;
+	for (int i = 0; i < size; i++)
+		commandLine += array[i];
+	if (CreateProcess(NULL, child, NULL, NULL, NULL, NULL, NULL, NULL, &sti, &pi)) {
+		
+		WaitForSingleObject(pi.hProcess,INFINITE);
+		CloseHandle(pi.hThread);
+		CloseHandle(pi.hProcess);
+	};
+	return 0;
+}

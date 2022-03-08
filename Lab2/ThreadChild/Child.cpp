6d5d22937b1340eb6c478a411f5a462f80b9d7b1
yanomaly <family.tolstoy@gmail.com>
2022-03-08 18:24:10 +0300

    Lab2 little changes

--- Lab2/ThreadChild/Child.cpp
+++ Lab2/ThreadChild/Child.cpp
@@ -4,25 +4,25 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-		int size = atoi(argv[1]), elem = atoi(argv[0]), j = 0, k = 0;
-		int* array = new int[size];
-		int* newArray = new int[size];
-		int* tempArray = new int[size];
-		for (int i = 0; i < size; i++)
-			array[i] = atoi(argv[2 + i]);
-		for (int i = 0; i < size; i++)
-			if (array[i] == elem) {
-				newArray[j] = elem;
-				j++;
-			}
-			else {
-				tempArray[k] = array[i];
-				k++;
-			}
-		for (int i = j, l = 0; l < k; l++, i++)
-			newArray[i] = tempArray[l];
-		for (int i = 0; i < size; i++)
-			cout << newArray[i] << " ";
-		Sleep(5000);
+	int size = atoi(argv[1]), elem = atoi(argv[0]), j = 0, k = 0;
+	int* array = new int[size];
+	int* newArray = new int[size];
+	int* tempArray = new int[size];
+	for (int i = 0; i < size; i++)
+		array[i] = atoi(argv[2 + i]);
+	for (int i = 0; i < size; i++)
+		if (array[i] == elem) {
+			newArray[j] = elem;
+			j++;
+		}
+		else {
+			tempArray[k] = array[i];
+			k++;
+		}
+	for (int i = j, l = 0; l < k; l++, i++)
+		newArray[i] = tempArray[l];
+	for (int i = 0; i < size; i++)
+		cout << newArray[i] << " ";
+	Sleep(5000);
 	return 0;
 }
